fff61122d1b87bdd2fed2f3bab048636086bd328
cpele <geekarist@users.noreply.github.com>
2017-06-20 18:43:59 +0200

    Use a ViewModel

---
+++ app/src/main/java/fr/xebia/cpele/xebiablog/FeedViewModel.java
@@ -0,0 +1,56 @@
+package fr.xebia.cpele.xebiablog;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+import retrofit2.http.GET;
+
+public class FeedViewModel extends ViewModel {
+
+    private static final String FEED_URL = "http://blog.xebia.fr";
+
+    private MutableLiveData<Feed> mFeed = new MutableLiveData<>();
+
+    public LiveData<Feed> getFeed() {
+        return mFeed;
+    }
+
+    public void init() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(FEED_URL)
+                .addConverterFactory(SimpleXmlConverterFactory.createNonStrict())
+                .build();
+
+        BlogApi blogApi = retrofit.create(BlogApi.class);
+
+        blogApi.fetchFeed().enqueue(new Callback<Feed>() {
+
+            @Override
+            public void onResponse(@NonNull final Call<Feed> call, @NonNull final Response<Feed> response) {
+                Feed feed = response.body();
+                Log.d(this.getClass().getSimpleName(), "Feed: " + feed);
+                mFeed.setValue(feed);
+            }
+
+            @Override
+            public void onFailure(@NonNull final Call<Feed> call, @NonNull final Throwable t) {
+                Log.e(this.getClass().getSimpleName(), "Here is an error", t);
+            }
+        });
+
+    }
+
+    private interface BlogApi {
+        @GET("/feed")
+        Call<Feed> fetchFeed();
+    }
+}
--- app/src/main/java/fr/xebia/cpele/xebiablog/MainActivity.java
+++ app/src/main/java/fr/xebia/cpele/xebiablog/MainActivity.java
@@ -1,11 +1,14 @@
 package fr.xebia.cpele.xebiablog;
 
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -18,16 +21,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
-import retrofit2.http.GET;
-
-public class MainActivity extends AppCompatActivity {
-
-    private static final String FEED_URL = "http://blog.xebia.fr";
+public class MainActivity extends LifecycleActivity {
 
     private ItemAdapter mAdapter;
 
@@ -41,41 +35,21 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(FEED_URL)
-                .addConverterFactory(SimpleXmlConverterFactory.createNonStrict())
-                .build();
+        FeedViewModel feedViewModel = ViewModelProviders.of(this).get(FeedViewModel.class);
 
-        BlogApi blogApi = retrofit.create(BlogApi.class);
+        feedViewModel.init();
 
-        blogApi.fetchFeed().enqueue(new Callback<Feed>() {
-
+        feedViewModel.getFeed().observe(this, new Observer<Feed>() {
             @Override
-            public void onResponse(@NonNull final Call<Feed> call, @NonNull final Response<Feed> response) {
-                Feed feed = response.body();
-                Log.d(MainActivity.this.getClass().getSimpleName(), "Feed: " + feed);
+            public void onChanged(@Nullable final Feed feed) {
                 if (feed != null && feed.channel.items != null) {
                     for (Item item : feed.channel.items) {
-                        Log.d(MainActivity.this.getClass().getSimpleName(), String.valueOf(item));
+                        Log.d(this.getClass().getSimpleName(), String.valueOf(item));
                     }
-
-                    mAdapter.addAll(feed.channel.items);
-                    mAdapter.notifyDataSetChanged();
                 }
                 Toast.makeText(MainActivity.this, "It's working", Toast.LENGTH_SHORT).show();
             }
-
-            @Override
-            public void onFailure(@NonNull final Call<Feed> call, @NonNull final Throwable t) {
-                Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
-                Log.e(MainActivity.this.getClass().getSimpleName(), "Here is an error", t);
-            }
         });
-    }
-
-    private interface BlogApi {
-        @GET("/feed")
-        Call<Feed> fetchFeed();
     }
 
     private static class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
